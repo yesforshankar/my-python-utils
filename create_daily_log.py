9eafca35cbbec81a2248b4a683d6cc30873d53a2
Shankar Jaganathan <thisisshankar@gmail.com>
2020-06-07 09:46:37 -0400

    Update June 07

--- create_daily_log.py
+++ create_daily_log.py
@@ -5,10 +5,7 @@ from os import path
 
 def file_exists(fn):
     file_valid = open(fn, "a")
-    # print ("Name of the file: ", file_valid.name)
-    # print ("Closed or not : ", file_valid.closed)
-    # print ("Opening mode : ", file_valid.mode)
-    # print("Current cursor position is : " + str(file_valid.tell()))
+    print("Current cursor position is : " + str(file_valid.tell()))
     if (file_valid.tell() == 0):
         print("Empty file")
     else:
@@ -24,7 +21,6 @@ def log_write(fname):
         header_string_text = ('Day # ' + str(i+1) + " / " + str(total_days) + ' - ' + var_date + ' (' + str(total_days - i-1) + " days remaining) ")
         header_string="\n"+"-"*len(header_string_text)+"\n"+header_string_text
         header_string=header_string+"\n"+"-"*len(header_string_text)+"\n\n"
-        # print(header_string)
         fo.write(header_string)
         
 total_days = int(input("Enter total_days for log : "))
